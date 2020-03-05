b56d50c28f9f3f251aa8ad8d102a052faefa672f
bharath-rg <bharath.kumar@ducenit.com>
2020-03-04 17:29:14 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-1firsttaskprompt34
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-1firsttaskprompt34)
---
+++ index.js
@@ -0,0 +1,5 @@
+//let a= prompt("enter your name");
+// b=prompt("start slice 2")
+// c=prompt("end slice 4")
+//alert(`${a.slice(b,c)}`);
+
