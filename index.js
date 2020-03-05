db408e9fc916483988b150c6120f06147765cf48
bharath-rg <bharath.kumar@ducenit.com>
2020-03-04 17:31:58 -0800

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//let a= prompt("enter your name");
-// b=prompt("start slice 2")
-// c=prompt("end slice 4")
-//alert(`${a.slice(b,c)}`);
+let a= prompt("enter your name");
+ let b=prompt("start slice 2")
+ let c=prompt("end slice 4")
+alert(`${a.slice(b,c)}`);
 
